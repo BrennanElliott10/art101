ac86e588a628575daae41c886ad7360b3138cdeb
Brennan Elliott <brennanelliott10@gmail.com>
2021-01-28 02:35:29 -0800

    lab 5 accord

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+ *Author: Brennan Elliott <brtellio@ucsc.edu>
+ *Created: 27 January
+ *License: Public Domain
+ */
+
+ // Define Variables
+ make = "Honda";
+ model = "Accord";
+ color = "Gray";
+ year = 2009;
+
+ // Calculate
+ age = 2021 - year;
+
+ //Output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years <br>");
